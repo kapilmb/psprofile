d9671c0def5ab14afbb6181f141a142935d545d2
Kapil Borle <kborle@microsoft.com>
2016-07-29 12:48:13 -0700

    Remove redundant get-prompt function

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,25 +1,4 @@
 # Set the prompt
-Function Get-Prompt1
-{
-    $maxPathLength = 16
-
-    $sb = [System.Text.StringBuilder]::new()
-    $sb.Append("PS ") | Out-Null
-    if ($pwd.Path.Length -gt $maxPathLength)
-    {
-        $sb.Append([System.IO.Path]::GetPathRoot($pwd)) | Out-Null
-        $sb.Append("...") | Out-Null
-        $sb.Append([System.IO.Path]::DirectorySeparatorChar) | Out-Null
-        $sb.Append([System.IO.Path]::GetFileName($pwd)) | Out-Null
-    }
-    else
-    {
-        $sb.Append($pwd.Path) | Out-Null
-    }
-    $sb.Append("> ") | Out-Null
-    $sb.ToString()
-}
-
 Function Get-Prompt
 {
     $sb = [System.Text.StringBuilder]::new()
